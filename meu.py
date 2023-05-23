71b26ce3665f95923ae312f830528b80f87b8b54
natanapa <natanafellipe2805@gmail.com>
2023-05-23 19:40:46 -0300

    VAMO LÁ

--- meu.py
+++ meu.py
@@ -6,7 +6,7 @@ from flask import Flask, make_response, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['POST', 'GET'])
+@app.route('/', methods = ['GET', 'POST'])
 def index():
     if request.method =='POST':
         return redirect('index.html')
